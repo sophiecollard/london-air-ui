2f62464187529fb7b78394024b88ad0e62d103fd
Sophie Collard <sophie.collard@hotmail.fr>
2023-10-12 23:26:45 +0100

    Load daily air quality data

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,10 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div, h1, text)
+import Html exposing (Html, div, h1, li, p, text, ul)
 import Html.Attributes exposing (class)
+import Http
+import Json.Decode exposing (Decoder, andThen, field, oneOf)
 
 
 
@@ -22,13 +24,179 @@ main =
 -- Model & init
 
 
-type alias Model =
-    ()
+type Model
+    = Loading
+    | Loaded DailyAirQualityData
+    | Error String
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( (), Cmd.none )
+    ( Loading, getDailyAirQualityData )
+
+
+type alias DailyAirQualityData =
+    { groupName : String
+    , localAuthority : List LocalAuthority
+    }
+
+
+dailyAirQualityDataDecoder : Decoder DailyAirQualityData
+dailyAirQualityDataDecoder =
+    let
+        innerDecoder =
+            Json.Decode.map2 DailyAirQualityData
+                (field "@GroupName" Json.Decode.string)
+                (field "LocalAuthority" (Json.Decode.list localAuthorityDecoder))
+    in
+    Json.Decode.map identity
+        (field "DailyAirQualityIndex" innerDecoder)
+
+
+type alias LocalAuthority =
+    { code : String
+    , name : String
+    , lat : Float
+    , lng : Float
+    , site : List Site
+    }
+
+
+localAuthorityDecoder : Decoder LocalAuthority
+localAuthorityDecoder =
+    Json.Decode.map5 LocalAuthority
+        (field "@LocalAuthorityCode" Json.Decode.string)
+        (field "@LocalAuthorityName" Json.Decode.string)
+        (field "@LaCentreLatitude" Json.Decode.string |> andThen floatAsStringDecoder)
+        (field "@LaCentreLongitude" Json.Decode.string |> andThen floatAsStringDecoder)
+        (oneOf
+            [ field "Site" (Json.Decode.list siteDecoder)
+            , Json.Decode.succeed []
+            ]
+        )
+
+
+type alias Site =
+    { code : String
+    , name : String
+    , lat : Float
+    , lng : Float
+    , species : List Species
+    }
+
+
+siteDecoder : Decoder Site
+siteDecoder =
+    Json.Decode.map5 Site
+        (field "@SiteCode" Json.Decode.string)
+        (field "@SiteName" Json.Decode.string)
+        (field "@Latitude" (Json.Decode.string |> andThen floatAsStringDecoder))
+        (field "@Longitude" (Json.Decode.string |> andThen floatAsStringDecoder))
+        (oneOf
+            [ field "Species" (Json.Decode.list speciesDecoder)
+            , Json.Decode.succeed []
+            ]
+        )
+
+
+type alias Species =
+    { code : SpeciesCode
+    , airQualityIndex : AirQualityIndex
+    , airQualityBand : String
+    , source : String
+    }
+
+
+speciesDecoder : Decoder Species
+speciesDecoder =
+    Json.Decode.map4 Species
+        (field "@SpeciesCode" speciesCodeDecoder)
+        (field "@AirQualityIndex" airQualityIndexDecoder)
+        (field "@AirQualityBand" Json.Decode.string)
+        (field "@IndexSource" Json.Decode.string)
+
+
+type AirQualityIndex
+    = Low
+    | Moderate
+    | High
+    | VeryHigh
+
+
+airQualityIndexDecoder : Decoder AirQualityIndex
+airQualityIndexDecoder =
+    let
+        fromString str =
+            case str of
+                "Low" ->
+                    Json.Decode.succeed Low
+
+                "Moderate" ->
+                    Json.Decode.succeed Moderate
+
+                "High" ->
+                    Json.Decode.succeed High
+
+                "Very High" ->
+                    Json.Decode.succeed VeryHigh
+
+                other ->
+                    Json.Decode.fail ("Failed to decode air quality index from " ++ other)
+    in
+    Json.Decode.string |> andThen fromString
+
+
+type SpeciesCode
+    = CO
+    | NO2
+    | O3
+    | PM10
+    | PM25
+    | SO2
+
+
+allSpeciesCode : List SpeciesCode
+allSpeciesCode =
+    [ CO, NO2, O3, PM10, PM25, SO2 ]
+
+
+speciesCodeDecoder : Decoder SpeciesCode
+speciesCodeDecoder =
+    let
+        fromString str =
+            case str of
+                "CO" ->
+                    Json.Decode.succeed CO
+
+                "NO2" ->
+                    Json.Decode.succeed NO2
+
+                "O3" ->
+                    Json.Decode.succeed O3
+
+                "PM10" ->
+                    Json.Decode.succeed PM10
+
+                "PM25" ->
+                    Json.Decode.succeed PM25
+
+                "SO2" ->
+                    Json.Decode.succeed SO2
+
+                other ->
+                    Json.Decode.fail ("Failed to decode species code from " ++ other)
+    in
+    Json.Decode.string |> andThen fromString
+
+
+floatAsStringDecoder : String -> Decoder Float
+floatAsStringDecoder str =
+    case String.toFloat str of
+        Just float ->
+            Json.Decode.succeed float
+
+        Nothing ->
+            Json.Decode.fail ("Failed to decode float from " ++ str)
 
 
 
@@ -36,14 +204,60 @@ init _ =
 
 
 type Msg
-    = DoNothing
+    = GetDailyAirQualityData
+    | GotDailyAirQualityData (Result Http.Error DailyAirQualityData)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        DoNothing ->
-            ( model, Cmd.none )
+        GetDailyAirQualityData ->
+            ( model, getDailyAirQualityData )
+
+        GotDailyAirQualityData result ->
+            case result of
+                Ok dailyAirQualityData ->
+                    ( Loaded dailyAirQualityData
+                    , Cmd.none
+                    )
+
+                Err (Http.BadBody bodyError) ->
+                    ( Error ("Encountered bad body error: " ++ bodyError)
+                    , Cmd.none
+                    )
+
+                Err (Http.BadStatus status) ->
+                    ( Error ("Encountered bad status error: " ++ String.fromInt status)
+                    , Cmd.none
+                    )
+
+                Err (Http.BadUrl url) ->
+                    ( Error ("Encountered bad URL error: " ++ url)
+                    , Cmd.none
+                    )
+
+                Err Http.NetworkError ->
+                    ( Error "Encountered network error"
+                    , Cmd.none
+                    )
+
+                Err Http.Timeout ->
+                    ( Error "Encountered timeout error"
+                    , Cmd.none
+                    )
+
+
+getDailyAirQualityData : Cmd Msg
+getDailyAirQualityData =
+    Http.get
+        { url = dailyAirQualityDataUrl
+        , expect = Http.expectJson GotDailyAirQualityData dailyAirQualityDataDecoder
+        }
+
+
+dailyAirQualityDataUrl : String
+dailyAirQualityDataUrl =
+    "https://api.erg.ic.ac.uk/AirQuality/Daily/MonitoringIndex/Latest/GroupName=London/Json"
 
 
 
@@ -60,5 +274,25 @@ subscriptions _ =
 
 
 view : Model -> Html Msg
-view _ =
-    div [] [ h1 [ class "title is-1" ] [ text "Hello, World!" ] ]
+view model =
+    let
+        contents =
+            case model of
+                Loading ->
+                    p [] [ text "Loading data" ]
+
+                Loaded dailyAirQualityData ->
+                    div []
+                        [ p [] [ text ("Got daily air quality index for " ++ dailyAirQualityData.groupName ++ ", with locales:") ]
+                        , ul [] (List.map (\locale -> li [] [ text locale.name ]) dailyAirQualityData.localAuthority)
+                        ]
+
+                Error msg ->
+                    p [] [ text msg ]
+    in
+    div [ class "container" ]
+        [ div [ class "content" ]
+            [ h1 [ class "title is-1" ] [ text "London Air Quality" ]
+            , contents
+            ]
+        ]
